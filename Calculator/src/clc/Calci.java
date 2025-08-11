4af1ec1b927f687edc0592eda184084be79d27b0
ShaikAshraf18 <ashrafvirat43@gmail.com>
2025-08-11 15:58:11 +0530

    1-Modificaition-by-TL

---
+++ Calculator/src/clc/Calci.java
@@ -0,0 +1,19 @@
+package clc;
+
+public class Calci {
+
+	
+	public int add(int a,int b)
+	{
+	   return a+b;	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Calci c=new Calci();   
+		int num=c.add(1,2);
+		System.out.println(num);
+		
+	}
+
+}
