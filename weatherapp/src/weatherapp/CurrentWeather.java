3ebdd8a2efe820a8879446650d60d56bd134e6e0
ShaikAshraf18 <ashrafvirat43@gmail.com>
2025-08-13 15:33:17 +0530

    2-commit by dl1

--- weatherapp/src/weatherapp/CurrentWeather.java
+++ weatherapp/src/weatherapp/CurrentWeather.java
@@ -1,11 +1,16 @@
 package weatherapp;
 
+import java.util.Scanner;
+
 public class CurrentWeather {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
         System.out.println("Currently the weather in nalgonda is cloudy and cloud");
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        
 	}
 
 }
